2ebba040c2280dbc8facc33001306ee594fd5e05
Philippe MICHEL <philippe.michel1@gmail.com>
2024-05-11 13:38:24 +0200

    Initial Commit

---
+++ crazyballs/crazyballsApp.swift
@@ -0,0 +1,17 @@
+//
+//  crazyballsApp.swift
+//  crazyballs
+//
+//  Created by Philippe MICHEL on 11/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct crazyballsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
