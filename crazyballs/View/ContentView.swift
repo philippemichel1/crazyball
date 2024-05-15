0299346030498d8ad4daf3ff8b8d6ca52ef99c18
Philippe MICHEL <philippe.michel1@gmail.com>
2024-05-15 14:52:50 +0200

    organisation du formulaire de paramèttre

--- crazyballs/View/ContentView.swift
+++ crazyballs/View/ContentView.swift
@@ -33,48 +33,45 @@ struct ContentView: View {
                         .frame(width: 50, height: 50)
                         .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
                         .shadow(radius:10).opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
-                    
                 }
-                Spacer(minLength: 5)
+               
+                Spacer()
                 Form {
-                    VStack (alignment: .leading, spacing: 1) {
-                        Section("Horizontal") {
-                            Stepper("Position.x : \(valuePositionX)", value: $valuePositionX, in: 0...10)
-                        }
-                        VStack (alignment: .leading) {
-                            Spacer(minLength: 10)
-                            Section("Vertical") {
-                                Stepper("Position.y : \(valuePositionY)", value: $valuePositionY, in: 0...10)
+                    Section(header: Text("Horizontal")) {
+                        Stepper("Position(x)  \(valuePositionX)", value: $valuePositionX, in: 0...10)
+                    } // fin de section
+                    
+                    Section(header: Text("Vertical")) {
+                        Stepper("Position(y)  \(valuePositionY)", value: $valuePositionY, in: 0...10)
+                    } // fin de section
+                    
+                    Section(header:Text("Direction")) {
+                            Toggle(movingRight ? "Vers la droite" : "vers la gauche", isOn: $movingRight)
+                                .tint(.blue)
+                            Toggle(movingDown ?"Vers le bas" : "Vers le haut", isOn: $movingDown)
+                                .tint(.blue)
+                        } // Fin de section
+                    
+                    Section(header:Text("Position Ball")) {
+                            Button(action: {
+                                self.showViewSetBall.toggle()
+                            }, label: {
+                                Text("Position de départ  de la Boule")
+                            })
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(.blue)
+                            .cornerRadius(10)
+                            .fullScreenCover(isPresented: $showViewSetBall) {
+                                MoveBall(settingP: self.settingP)
                             }
                         }
-                        
-                        Section("Direction") {
-                            Toggle(movingRight ? "Droite" : "Gauche", isOn: $movingRight)
-                                .tint(.blue)
-                            Toggle(movingDown ?"Bas" : "Haut", isOn: $movingDown)
-                                .tint(.blue)
-                        }
-                    }
-                    Section("Position") {
-                        Button(action: {
-                            self.showViewSetBall.toggle()
-                        }, label: {
-                            Text("Position de départ  de la Boule")
-                        })
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(.blue)
-                        .cornerRadius(10)
-                        .fullScreenCover(isPresented: $showViewSetBall) {
-                            MoveBall(settingP: self.settingP)
-                        }
-                    }.padding(.vertical)
-                }
-                
+                }// form
+                   // affiche le formulaire résultat
                 Button(action: {
                     self.renderingButton.toggle()
                 }, label: {
-                    Text("voir le rendu")
+                    Text("Lancer")
                 })
                 .padding()
                 .foregroundColor(.white)
@@ -83,7 +80,6 @@ struct ContentView: View {
                 .navigationDestination(isPresented: $renderingButton) {
                     RenderingView(valuePositionX: $valuePositionX, valuePositionY: $valuePositionY, newPosition:.constant(CGPoint(x:settingP.position.x,y:settingP.position.y)),movingRight: $movingRight,movingDown: $movingDown)
                 }
-                Spacer(minLength: 30)
                 .navigationTitle("Paramètrage")
             }
             .edgesIgnoringSafeArea(.all)
