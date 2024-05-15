90b57149a2be872b6c0fb955c0f127aaa7b71d8a
Philippe MICHEL <philippe.michel1@gmail.com>
2024-05-15 13:23:28 +0200

    commentaire du code

--- crazyballs/View/ContentView.swift
+++ crazyballs/View/ContentView.swift
@@ -9,24 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var position = CGPoint(x: 54, y: 31) // Position initiale
+    // bouton du rendu animation
     @State var renderingButton:Bool = false
-    @State var valuePositionX:Int = 5
-    @State var valuePositionY:Int = 5
+    // valeur changement de position horizontal de la boule
+    @State var valuePositionX:Int = 4
+    // valeur changement de position vertical de la boule
+    @State var valuePositionY:Int = 4
+    // affiche la fenetre de position de la boule
     @State var showViewSetBall:Bool = false
-    // déplacement vers la droite 
+    // déplacement vers la droite
     @State private var movingRight = true
     // déplacement vers le bas
     @State private var movingDown = true
+    //creation de l'object de recuperation de la position de la boule
     @State var settingP:SettingP = SettingP()
     
-    
-    
+
     var body: some View {
-       // GeometryReader { geometry in
             NavigationStack {
-                Spacer(minLength: 50)
+                VStack{
+                    Image("titastus")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        .shadow(radius:10).opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+                    
+                }
+                Spacer(minLength: 5)
                 Form {
-                    VStack (alignment: .leading) {
+                    VStack (alignment: .leading, spacing: 1) {
                         Section("Horizontal") {
                             Stepper("Position.x : \(valuePositionX)", value: $valuePositionX, in: 0...10)
                         }
@@ -43,13 +54,12 @@ struct ContentView: View {
                             Toggle(movingDown ?"Bas" : "Haut", isOn: $movingDown)
                                 .tint(.blue)
                         }
-                        
                     }
                     Section("Position") {
                         Button(action: {
                             self.showViewSetBall.toggle()
                         }, label: {
-                            Text("Position de la Boule")
+                            Text("Position de départ  de la Boule")
                         })
                         .padding()
                         .foregroundColor(.white)
@@ -58,7 +68,7 @@ struct ContentView: View {
                         .fullScreenCover(isPresented: $showViewSetBall) {
                             MoveBall(settingP: self.settingP)
                         }
-                    }
+                    }.padding(.vertical)
                 }
                 
                 Button(action: {
@@ -73,14 +83,10 @@ struct ContentView: View {
                 .navigationDestination(isPresented: $renderingButton) {
                     RenderingView(valuePositionX: $valuePositionX, valuePositionY: $valuePositionY, newPosition:.constant(CGPoint(x:settingP.position.x,y:settingP.position.y)),movingRight: $movingRight,movingDown: $movingDown)
                 }
-                Spacer(minLength: 50)
+                Spacer(minLength: 30)
                 .navigationTitle("Paramètrage")
             }
             .edgesIgnoringSafeArea(.all)
-       // }
-       
-        
-        
     }
 }
 
